0cfc17c02561a7e8b845bcb428c7353b57be96a2
tarowillmakeitright <ryutaro.maegima@gmail.com>
2024-10-26 08:38:51 +0900

    added bootstrap, fontawesome

--- src/main/java/com/ecommerce/ecommerce/HomeController.java
+++ src/main/java/com/ecommerce/ecommerce/HomeController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
@@ -16,4 +19,11 @@ public class HomeController {
     public String product(Model model) {
         return "product";
     }   
+    @PostMapping("/product")
+    @ResponseBody
+    public String backTextProduct(@RequestParam("prompt") String hxPrompt) {
+        return "<p>Received: " + hxPrompt + "</p>";
+    }
+
+
 }
